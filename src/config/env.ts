785d6dfbadd9a731b6f1700c6f15a49023235774
nvnx7 <thenvnsahu@gmail.com>
2026-05-07 20:11:59 +0530

    chore: add env vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const rollupChainId = 6669;
-export const rollupEndpoint = "http://localhost:12346";
+export const rollupEndpoint = process.env.NEXT_PUBLIC_ROLLUP_ENDPOINT as string;
 export const rollupApiBaseUrl = `${rollupEndpoint}/modules`;
-export const indexerApiBaseUrl = "http://localhost:4000/api/v1";
+export const indexerApiBaseUrl = process.env.NEXT_PUBLIC_INDEXER_API_BASE_URL as string;
 export const testUserPrivateKey = process.env.NEXT_PUBLIC_TEST_USER_PRIVATE_KEY as string;
