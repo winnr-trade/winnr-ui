53346465d4f585ea2b52d87f5de1f18210084604
nvnx7 <thenvnsahu@gmail.com>
2026-05-08 14:22:06 +0530

    refactor: use `Image` component instead of `img`

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,19 +1,23 @@
+import Image from "next/image";
 import { cn } from "@/utils/cn";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center", className)}>
-      <img
+      <Image
         src="/logo.svg"
         alt="Winnr"
+        width={25}
+        height={28}
         className="block md:hidden h-7 w-auto"
       />
-      <img
+      <Image
         src="/logo-wide.svg"
         alt="Winnr"
+        width={88}
+        height={28}
         className="hidden md:block h-7 w-auto"
       />
     </div>
   );
 }
-
